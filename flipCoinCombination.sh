967a0b67923f20965cfb671e1ad03d35a2c713dc
AniketGujar <gujaraniket7875@gmail.com>
2020-11-04 16:33:47 +0530

    Added: Flipping the Coin and displaying the result

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,3 +2,11 @@
 
 echo "This problem displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet"
 
+toss=$((RANDOM%2))
+
+if [[ $toss -eq 1 ]]
+then
+	echo "Heads"
+else
+	echo "Tails"
+fi
