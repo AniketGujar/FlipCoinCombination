860a8435d5ca859923e42b01fa43af63d96fb289
AniketGujar <gujaraniket7875@gmail.com>
2020-11-04 17:09:17 +0530

    Added: Singlet Coin Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
 echo "This problem displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet"
+echo "Singlet Combination - H / T"
 
+declare -A singlet
+for (( i=1; i<=21; i++))
+do
 toss=$((RANDOM%2))
 
 if [[ $toss -eq 1 ]]
 then
-	echo "Heads"
+	echo "H"
+	((h++))
+	singlet[Heads]=$h
 else
-	echo "Tails"
+	echo "T"
+	((t++))
+	singlet[Tails]=$t
+fi
+done
+
+if [[ $h -gt $t ]]
+then
+	hp=`expr $h \* 100 / 20`
+	echo "H Win with $hp %"
+else
+        tp=`expr $t \* 100 / 20`
+        echo "T Win with $tp %"
+
 fi
